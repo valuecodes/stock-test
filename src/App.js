2929bb45f6588bb4be31b8f139eb59a37596c2d6
Juha Kangas <juhakangas5@gmail.com>
2021-01-28 16:20:42 +0200

    Added data fetching and parsing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
+import { csvToJSON } from './utils';
 
 function App() {
+
+  const [stockData, setStockData] = useState(null)
+
+  useEffect(() => {  
+    const fetchData = async () =>{
+      const csvData = await fetchCsv()
+      const jsonData = csvToJSON(csvData)
+      setStockData(jsonData)
+    }
+    fetchData()
+  }, [])
+
+  async function fetchCsv() {
+    const response = await fetch('data.csv');
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder('utf-8');
+    const csv = await decoder.decode(result.value);
+    return csv;
+  }
+  console.log(stockData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
     </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+export const csvToJSON = (csvData) =>{
+    const rows = csvData.split('\n')
+    const header = rows[0].split(', ')
+    const dataRows = rows.filter((item,index) => index !== 0&& item !== '') 
+    const jsonData = dataRows.map(row => {
+        let column = row.split(', ')
+        let jsonColumn = {}
+        header.forEach((item,index) => jsonColumn[item] = parseValue(column[index],item))
+        return jsonColumn
+    });
+    return jsonData
+}
+
+export function parseValue(value,type){
+    switch(type){
+        case 'Date':
+            return new Date(value)
+        default: 
+            return Number(value.replace(/[&\/\\#,+()$~%'":*?<>{}]/g, ''))
+    }
+}
